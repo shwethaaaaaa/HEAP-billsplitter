894e455af01927b59bcb7fb17eb59dbccdbe9eb3
Sanofer11 <sanofer.2020@scis.smu.edu.sg>
2022-06-15 21:23:19 +0800

    more UI components(inc) & billsplitter logo

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Footer from './components/Footer.js';
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import AddingNewGroup from './components/Group.js';
-import AddTransaction from './components/Transaction.js'
+import AddingNewGroup from './components/AddGroup.js';
+import AddTransaction from './components/AddTransaction.js'
+import TransactionTable from './components/TransactionTable.js';
+
+
 
 
 function App() {
   return (
     <div className="App">
+      
       <Button variant="contained" color="secondary">Add Group</Button>
+      <TransactionTable></TransactionTable>
       <hr/>
       <AddingNewGroup></AddingNewGroup>
       <hr/>
       <AddTransaction></AddTransaction>
+      <Footer></Footer>
 
 
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,23 @@
+
+
+import React from "react";
+
+const style = {
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
 export default function Footer() {
-    return(
-        <>
-        </>
-    );
+  return (
+    <div>
+      <div style={style}>hello</div>
+    </div>
+  );
 }
