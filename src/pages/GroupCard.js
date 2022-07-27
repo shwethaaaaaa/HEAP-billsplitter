c8d4dd77a978aa52af43174bab88995fd2c15bcd
shwethaaaaaa <89060134+shwethaaaaaa@users.noreply.github.com>
2022-07-27 13:24:53 +0800

    added small image in cards via axios

--- src/pages/GroupCard.js
+++ src/pages/GroupCard.js
@@ -3,6 +3,9 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import useFetch from './useFetch';
+import axios from 'axios';
+import React from 'react';
 
 
 
@@ -10,15 +13,36 @@ import Col from 'react-bootstrap/Col';
 export default function MyGroupCards({mygroupsdata}){
     const MyGroups =  mygroupsdata.data.groups
     console.log(MyGroups)
+
+    for (const group of MyGroups){
+        const trip_location = group.group_name
+        const Unsplash_API_URL = 'https://api.unsplash.com/search/photos?client_id=Tnkm6qLI-aFFmxeB_YxL_lZfMwTNqX4W9T7JhYjqHfg&query=' + trip_location + '&orientation=landscape'
+      
+            axios.get(Unsplash_API_URL)
+              .then(res => {
+                
+                console.log(res.data.results[0].urls.small)
+                const small_image_url = res.data.results[0].urls.small;
+                group['image_url'] = small_image_url
+                console.log(group)
+                
+              })
+            
+          
+        // const small_image_url = unsplashdata.results[0].urls.small
+        // group['image_url'] = small_image_url
+        // console.log(group)
+    }
+
+    console.log(MyGroups)
     return(
          <>
              <Container className='mt-5'>
                 {MyGroups.map((option) => (
-                   
                     <Row className="ms-4" >
                         <Col className="mb-3" >
                             <Card style={{ width: '18rem' }} className='my-auto'>
-                                        <Card.Img variant="top" src={require('./friendss.jpg')}/>
+                                        <Card.Img variant="top" src= {option.image_url}/>
                                         <Card.Body>
                                             <Card.Title>{option.group_name}</Card.Title>
                                             <Card.Text>Trip Duration: <b>{option.trip_duration}</b></Card.Text>
